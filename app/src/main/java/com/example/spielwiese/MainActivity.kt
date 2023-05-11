fb54bf71bdc6ebcf608da81d389b5884650ba4c4
Justin Sprenger <justin_sprenger@hotmail.de>
2023-05-11 23:24:40 +0200

    create a useless button

--- app/src/main/java/com/example/spielwiese/MainActivity.kt
+++ app/src/main/java/com/example/spielwiese/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.spielwiese
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Switch
+import android.widget.Toast
+import com.example.spielwiese.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +18,11 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("name", "$deinName")
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.uselessButton.setOnClickListener {
+            Toast.makeText(this,"test", Toast.LENGTH_SHORT).show()
+        }
     }
 }
