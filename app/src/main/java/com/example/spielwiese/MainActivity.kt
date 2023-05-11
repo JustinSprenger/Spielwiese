da0616119352173ebe629d233a6d362eded4262e
Kelvak <Gk1_1993@gmx.de>
2023-05-11 23:09:41 +0200

    feature/switch:: implemented switch

--- app/src/main/java/com/example/spielwiese/MainActivity.kt
+++ app/src/main/java/com/example/spielwiese/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.spielwiese
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Switch
+import com.example.spielwiese.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +18,30 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("name", "$deinName")
 
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        val view = binding.root
+        setContentView(view)
+
+        binding.txtMain.setText("An");
+
+        binding.swtText.setOnCheckedChangeListener { _, isChecked ->
+            switch(isChecked)
+        };
+
     }
+
+    private fun switch(switchOn: Boolean)
+    {
+        Log.d("Spielwiese","Test");
+        if(switchOn)
+        {
+            binding.txtMain.setText("An");
+        }
+        else{
+            binding.txtMain.setText("Aus");
+        }
+
+    }
+
+
 }
