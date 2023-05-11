34305cc51c1d89c6cca37548f254dc568a0494f8
Justin Sprenger <justin_sprenger@hotmail.de>
2023-05-11 22:19:24 +0200

    minor stuff

--- app/src/main/java/com/example/spielwiese/MainActivity.kt
+++ app/src/main/java/com/example/spielwiese/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.spielwiese
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val deinName : String = "Hans"
+
+        Log.d("name", "$deinName")
+
     }
 }
